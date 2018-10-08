20f1404e765d6a506b296afa52f9dd567eb7c05b
Steven Brookes <steven.brookes@talisman-innovations.com>
2018-10-08 16:03:18 +0100

    GENT-530 Switch to utf8mb4 as default character set and collation

--- src/Entity/DoctrineMessage.php
+++ src/Entity/DoctrineMessage.php
@@ -33,6 +33,7 @@ use Talisman\TideBundle\Traits\TransactionTrait;
 /**
  * @ORM\Entity(repositoryClass="Uecode\Bundle\QPushBundle\Repository\DoctrineRepository")
  * @ORM\Table(name="uecode_qpush_message",
+ * options={"collate":"utf8_general_ci", "charset":"utf8"},
  * indexes={@ORM\Index(name="uecode_qpush_queue_idx",columns={"queue"}),
  *          @ORM\Index(name="uecode_qpush_delivered_idx",columns={"delivered"}),
  *          @ORM\Index(name="uecode_qpush_created_idx",columns={"created"}),
